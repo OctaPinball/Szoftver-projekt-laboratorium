eb88f33e24109fe505f47442e0c5899503de70af
davidpataki0 <patakidavid2001@gmail.com>
2022-03-28 11:56:17 +0200

    [Biti] FillMaterial javadocolva

--- src/fillmaterial/FillMaterial.java
+++ src/fillmaterial/FillMaterial.java
@@ -3,6 +3,11 @@ package fillmaterial;
 import miscellaneous.Virologist;
 
 public interface FillMaterial {
+	
+    /**
+     * A paraméterként kapott virológus nyersanyagainak feltöltéséhez szükséges
+     * @param v, a virológus, aki szeretne nyersanyagot
+     */
 
 	public abstract void fillMaterial(Virologist v);
 	
