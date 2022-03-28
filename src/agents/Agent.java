332ef253ac141147a34b44381a4b93fbae7c3eee
OctaPinball <scholtz.balint@gmail.com>
2022-03-28 10:21:37 +0200

    [Gazda] Agent Merge Issues Solved

--- src/agents/Agent.java
+++ src/agents/Agent.java
@@ -1,5 +1,52 @@
 package agents;
 
-public abstract class Agent {
+import java.util.ArrayList;
 
+import miscellaneous.Logger;
+import miscellaneous.Virologist;
+
+public abstract class Agent {
+	private int acidcost;
+	private int nucleotidecost;
+	private int effecttime;
+	
+	protected Virologist owner;
+	
+	Agent(){
+		
+	}
+	
+	public void cast(Virologist target, int i) throws CloneNotSupportedException {
+		ArrayList<Object> par = new ArrayList<>(); par.add(target); par.add(i);
+		Logger.enter(this, "cast", par);
+		
+		if(owner.getAminoacid() - acidcost < 0 || owner.getNucleotide() - nucleotidecost < 0)
+			return;
+		owner.setAminoAcid(owner.getAminoacid() - acidcost);
+		owner.setNucleotide(owner.getNucleotide() - nucleotidecost);
+		
+		if(!target.getBlock().block(owner, target, this))
+		{
+			Agent copy = (Agent) this.clone();
+			target.addActiveAgent(copy);
+		}
+		
+		Logger.exit(this, "cast", null);
+	}
+	
+	public abstract void activate();
+	
+	public abstract void deactivate();
+	
+	public void stepEffectTime() {
+		Logger.enter(this, "stepEffectTime", null);
+		
+		effecttime = effecttime - 1;
+		if (effecttime <= 0)
+			this.deactivate();
+		
+		Logger.exit(this, "stepEffectTime", null);
+	}
+	
+	
 }
--- src/agents/Protection.java
+++ src/agents/Protection.java
@@ -1,5 +1,24 @@
 package agents;
 
-public class Protection {
+import block.NoBlock;
+import block.PartialBlock;
+import miscellaneous.Logger;
 
+public class Protection extends Agent{
+	public void activate() {
+		Logger.enter(this, "activate", null);
+		
+		owner.setBlock(new PartialBlock());
+		
+		Logger.exit(this, "activate", null);
+	}
+	
+	public void deactivate() {
+		Logger.enter(this, "deactivate", null);
+		
+		owner.setBlock(new NoBlock());
+		owner.getActiveAgents().remove(this);
+		
+		Logger.exit(this, "deactivate", null);
+	}
 }
