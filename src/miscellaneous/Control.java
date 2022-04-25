0dcb99c0ce3f7db9aa4a7a920b8d8be33c95cd53
OctaPinball <scholtz.balint@gmail.com>
2022-04-25 12:40:00 +0200

    [Gazda] Chaos

--- src/miscellaneous/Control.java
+++ src/miscellaneous/Control.java
@@ -3,13 +3,10 @@ package miscellaneous;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-
 import agents.*;
 import equipment.*;
 import field.*;
