0db5b854fac96a70a62b03f55c2283e45338e2e4
imets01 <imets01@gmail.com>
2022-05-11 20:37:54 +0200

    Paint

--- src/graphic/GamePanel.java
+++ src/graphic/GamePanel.java
@@ -1,6 +1,10 @@
 package graphic;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -41,5 +45,16 @@ public class GamePanel extends JPanel implements Const{
 		j.setOpaque(true);
 		labels.add(j);
 	}
+	
+	public void paint(Graphics g) {
+	Graphics2D g2D = (Graphics2D)g;
+	g2D.setPaint(Color.red);
+	g2D.setFont(new Font("Arial", Font.BOLD, 16));
+	g2D.drawString("Inventory", 20, 576);
+	g2D.drawString("Agent", 200, 576);
+	g2D.drawString("Active agent", 300, 576);
+	g2D.drawString("materials", 420, 576);
+	
+}
 
 }
