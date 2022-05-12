dd3bafa61c606734499ce540a435ddb36a4a5018
OctaPinball <scholtz.balint@gmail.com>
2022-05-12 11:39:05 +0200

    [Gazda] Tester fix

--- src/graphic/InteractMenu.java
+++ src/graphic/InteractMenu.java
@@ -1,11 +1,15 @@
 package graphic;
 
 import agents.*;
+import equipment.*;
 import field.*;
-import miscellaneous.RoundManager;
+import miscellaneous.*;
+import movement.*;
 
 public class InteractMenu {
 	public InteractMenu(Field f) {
+		
+		Position relativepos = f.calculateCoordinates();
 		
 		for(Field field : RoundManager.getEntity().getField().getNeighbors())
 		{
@@ -15,25 +19,45 @@ public class InteractMenu {
 				{
 					
 				}
-				else
+				else if(!f.getVirologist().equals(RoundManager.getEntity()))
 				{
-					if(!f.getVirologist().equals(RoundManager.getEntity()))
+					
+					for(int i = 0; i < f.getVirologist().getAgents().size(); i++)
 					{
-						for(int i = 0; i < f.getVirologist().getAgents().size(); i++)
+						if(f.getVirologist().getAgents().get(i).getClass() == Chorea.class)
 						{
-							if(f.getVirologist().getAgents().get(i).getClass() == Chorea.class)
+								
+						}
+						if(f.getVirologist().getAgents().get(i).getClass() == ForgettingAgent.class)
+						{
+								
+						}
+						if(f.getVirologist().getAgents().get(i).getClass() == Protection.class)
+						{
+								
+						}
+						if(f.getVirologist().getAgents().get(i).getClass() == Stun.class)
+						{
+								
+						}
+					}
+					if(f.getVirologist().getMovement().getClass() == Stunned.class)
+					{
+						for(int i = 0; i < f.getVirologist().getEquipments().size(); i++)
+						{
+							if(f.getVirologist().getEquipments().get(i).getClass() == Axe.class)
 							{
 								
 							}
-							if(f.getVirologist().getAgents().get(i).getClass() == ForgettingAgent.class)
+							if(f.getVirologist().getEquipments().get(i).getClass() == Cape.class)
 							{
 								
 							}
-							if(f.getVirologist().getAgents().get(i).getClass() == Protection.class)
+							if(f.getVirologist().getEquipments().get(i).getClass() == Glove.class)
 							{
 								
 							}
-							if(f.getVirologist().getAgents().get(i).getClass() == Stun.class)
+							if(f.getVirologist().getEquipments().get(i).getClass() == Sack.class)
 							{
 								
 							}
