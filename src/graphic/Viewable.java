c8b6489e29ab2731da650c2e75edd0140b77a23d
davidpataki0 <patakidavid2001@gmail.com>
2022-05-16 13:53:49 +0200

    viewable komment

--- src/graphic/Viewable.java
+++ src/graphic/Viewable.java
@@ -1,7 +1,22 @@
 package graphic;
 
+/**
+ * A megjeleníthető elemek interfésze
+ */
 public interface Viewable {
+	
+	/**
+	 * Paraméterül kap egy View-t, amin meghívja az adott elem draw függvényét
+	 * @param v, a View paraméter
+	 */
+	
 	public void pickDraw(View v);
+	
+	/**
+	 * Visszatér az elem koordinátájával, pozíciójával
+	 * @return Position, a pozíció visszatérési értéke
+	 */
+	
 	public Position calculateCoordinates();
 }
 
