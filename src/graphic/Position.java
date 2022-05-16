c5ebf0926fcd885dbf24efcab3998f07758c3518
davidpataki0 <patakidavid2001@gmail.com>
2022-05-16 13:30:34 +0200

    pozi

--- src/graphic/Position.java
+++ src/graphic/Position.java
@@ -15,23 +15,48 @@ public class Position {
 		setX(x_in);
 		setY(y_in);
 	}
+	
+	/**
+	 * Visszatér a pozíció x-értékével
+	 * @return int, az x-értéke
+	 */
 
 	public int getX() {
 		return x;
 	}
+	
+	/**
+	 * Beállítja a pozíció x-értékét
+	 * @param x, az x kívánt értéke
+	 */
 
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Visszatér a pozíció y-értékével
+	 * @return int, az y-értéke
+	 */
+	
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Beállítja a pozíció y-értékét
+	 * @param x, az y kívánt értéke
+	 */
+	
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Visszatér a két pozíció egyezésével vagy nem egyezésével
+	 * @param p, az összehasonlítandó pozíció
+	 * @return boolean, az egyezés visszatérési értéke
+	 */
 	
 	public boolean Equals(Position p) {
 		return this.x == p.x && this.y == p.y;
