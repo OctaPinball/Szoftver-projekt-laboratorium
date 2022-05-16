6de85232262eca3fc72c272929ca5f10ccdd5ceb
davidpataki0 <patakidavid2001@gmail.com>
2022-05-16 12:54:35 +0200

    [Biti] Movementek kommentelve

--- src/movement/BearDance.java
+++ src/movement/BearDance.java
@@ -6,15 +6,26 @@ import field.Field;
 import field.Storage;
 import miscellaneous.Virologist;
 
+
+/**
+ * A movement interfész leszármazottja, ezt a mozgatást kapja meg a virológus, mikor medvévé változik.
+ */
 public class BearDance extends RandomMovement{
 	
 	private final int priority = 3;
 	
+    /**
+     * A paraméterként kapott virológus szeretne átlépni a paraméterként kapott mezőre
+     * @param v, a virológus, aki szeretne ellépni
+     * @param target, a mező, ahova lépni szeretne
+     * @return boolean, a lépés sikerességét adja vissza
+     * @throws IOException 
+     * @throws CloneNotSupportedException 
+     */
+	
 	public boolean move(Virologist v, Field target) throws CloneNotSupportedException, IOException {
 		
 		super.move(v, target);
-		
-		
 		
 		for(int i = 0; i < v.getField().getNeighbors().size(); i++) {
 			if(v.getField().getNeighbors().get(i).getVirologist() != null)
@@ -22,6 +33,11 @@ public class BearDance extends RandomMovement{
 		}
 		return true;
 	}
+	
+	/**
+	 * A mozgás prioritási számát adja vissza
+	 * @return int, a prioritás száma
+	 */
 	
 	public int getPriority() {
 		return priority;
--- src/movement/Movement.java
+++ src/movement/Movement.java
@@ -14,12 +14,18 @@ public interface Movement {
      * A paraméterként kapott virológus szeretne átlépni egy szomszédos mezőre
      * @param v, a virológus, aki szeretne ellépni
      * @param target, a mező, ahova lépni szeretne
+     * @return boolean, a lépés sikerességét adja vissza
      * @throws CloneNotSupportedException 
      * @throws IOException 
      */
 	
 	public abstract boolean move(Virologist v, Field target) throws CloneNotSupportedException, IOException;
 
+	/**
+	 * A mozgás prioritási számát adja vissza
+	 * @return int, a prioritás száma
+	 */
+	
 	public abstract int getPriority();
 
 }
